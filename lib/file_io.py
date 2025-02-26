0854153ca0f018230d552e92161ba752423a80b0
wakio-ngari <ngarijoyce017@gmail.com>
2025-02-26 16:06:05 +0300

    completed file_io.py

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
+import os
 def write_file(file_name, file_content):
-    pass
-
+    with open(f"{file_name}.txt", "w") as file:#writes content to txt,
+        #if it already exists its overwritten
+        file.write(file_content)
+    
+#adds content to a txt file if it doesn't exist
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    if not os.path.exists(f"{file_name}.txt"):
+        return "File not found."
+    
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
+
+  
